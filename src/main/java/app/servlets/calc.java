e73cee2dc5bb0149cb5e58d09d10ed76af6008fa
d-kur <d-kurd@yandex.ru>
2019-10-10 04:03:11 +0300

    1.1

--- src/main/java/app/servlets/calc.java
+++ src/main/java/app/servlets/calc.java
@@ -1,20 +1,24 @@
 package app.servlets;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet
-
-
+@WebServlet(
+        name = "calc",
+        urlPatterns = {"/calc"}
+)
 
 public class calc extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher();
+        System.out.println();
     }
 }
