0fa848484d35ca0cd0e5b09cc0461f0873ce34ab
d-kur <d-kurd@yandex.ru>
2019-10-10 04:00:45 +0300

    beginning

---
+++ src/main/java/app/servlets/calc.java
@@ -0,0 +1,20 @@
+package app.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet
+
+
+
+public class calc extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+    }
+}
