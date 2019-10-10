c6a529294a1bc871b6581f4e0d64cc4bdde63fb7
d-kur <d-kurd@yandex.ru>
2019-10-10 05:13:35 +0300

    pochti

--- src/main/java/app/servlets/calc.java
+++ src/main/java/app/servlets/calc.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 @WebServlet(
         name = "calc",
@@ -18,7 +21,68 @@ public class calc extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        //RequestDispatcher requestDispatcher = request.getRequestDispatcher();
-        System.out.println(request);
+        PrintWriter out = response.getWriter();
+        String equation = request.getParameter("equation");
+        Map<String, String[]> http = request.getParameterMap();
+        Set<String> params= http.keySet();
+
+        String value = "";
+        Map<String, Object> map = new HashMap<>();
+        for (String s: params) {
+            if (!s.equals("equation")){
+                value = request.getParameter(s);
+                out.println(value);
+                if (value.matches("[a-zA-Z]+")) {
+                    map.put(s, value);
+                } else {
+                    int v = Integer.getInteger(value);
+                    map.put(s, v);
+                }
+            }
+        }
+
+        out.println(map.keySet());
+
+        Set<String> variables = map.keySet();
+        int i = 0;
+        Map<String, String> matches = new HashMap<>();
+        for (String s: variables) {
+            matches.put(s, "x"+Integer.toString(i));
+            i++;
+        }
+
+        for (String s: variables) {
+            equation = equation.replaceAll(s, matches.get(s));
+        }
+
+        //System.out.println(variables);
+
+        //System.out.println(matches.entrySet());
+
+        //System.out.println(equation);
+        Integer test = 1;
+        Map<String, Integer> mapa = new HashMap<>();
+        for (String s: variables) {
+            mapa.put(s, (int) (map.get(s).getClass()!=test.getClass()?map.get(map.get(s)):map.get(s)));
+        }
+
+        double[] v = new double[variables.size()];
+        i=0;
+        for (String s: variables) {
+            v[i] =  mapa.get(s);
+            i++;
+        }
+
+        //System.out.println(v[0]+" "+ v[1]+" "+v[2]+" "+v[3]);
+
+        ExpressionEstimator estimator=new ExpressionEstimator();
+        try {
+            estimator.compile(equation);
+
+            out.println( (int) Math.floor(estimator.calculate(v)) );
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
