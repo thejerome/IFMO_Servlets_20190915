9175452419ff4105b6ada0c79714c4bff55836c1
Ann <265107@niuitmo.ru>
2019-10-11 18:46:40 +0300

    done

---
+++ src/main/Java/com/sam/web/servlet/Main.java
@@ -0,0 +1,42 @@
+package com.sam.web.servlet;
+
+import java.io.File;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        int port = 8080;
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(port);
+
+        String webappDirLocation = "src/main/webapp/";
+        StandardContext ctx = (StandardContext) tomcat.addWebapp(
+                "", new File(webappDirLocation).getAbsolutePath()
+        );
+        System.out.println(
+                "configuring app with basedir: "
+                        + new File("./" + webappDirLocation).getAbsolutePath()
+        );
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+
+        File additionWebInfClasses = new File("build/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(
+                new DirResourceSet(
+                        resources,
+                        "/WEB-INF/classes",
+                        additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
---
+++ src/main/Java/com/sam/web/servlet/VarInMap.java
@@ -0,0 +1,27 @@
+package com.sam.web.servlet;
+
+import java.util.Map;
+
+public class VarInMap {
+    public static String inMap(Map<String, String[]> var, String equation){
+
+        while (isLetters(equation)) {
+            for (int i = 0; i < equation.length(); i++) {
+                char symbol = equation.charAt(i);
+                if (symbol >= 'a' && symbol <= 'z') {
+                    equation = equation.replace(String.valueOf(symbol), String.valueOf(var.get(String.valueOf(symbol))[0]));
+                }
+            }
+        }
+        return(equation);
+    }
+
+    private static boolean isLetters(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLetter(str.charAt(i))) return true;
+        }
+        return false;
+    }
+
+
+}
