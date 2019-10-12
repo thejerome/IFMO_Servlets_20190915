281d17e02f2d5723555419984fcf30f0ba98ed7e
Gena Ziganshin <gena.ziganshin@yandex.ru>
2019-10-12 19:47:29 +0300

    calc working   :)

--- src/main/java/CalcServlet.java
+++ src/main/java/CalcServlet.java
@@ -23,45 +23,43 @@ public class CalcServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
         out.println(calculate(req));
     }
-    private int calculate(HttpServletRequest req){
+
+    private String calculate(HttpServletRequest req) {
         Map<String, String[]> p = req.getParameterMap();
 
         String equation = toNormForm(p);
 
-        while (equation.indexOf('(') != -1){
+        while (equation.indexOf('(') != -1) {
             int leftBorder = equation.lastIndexOf('(');
             int rightBorder = equation.indexOf(')', leftBorder);
-            equation = doOperations(equation.substring(leftBorder, rightBorder + 1));
-            rightBorder = equation.indexOf(')', leftBorder);
-            if (rightBorder < equation.length() - 1){
+            if (rightBorder < equation.length() - 1)
                 equation = equation.substring(0,leftBorder) +
-                        equation.substring(leftBorder + 1, rightBorder) +
+                        doOperations(equation.substring(leftBorder, rightBorder + 1)) +
                         equation.substring(rightBorder + 1);
-            }else{
+            else
                 equation = equation.substring(0,leftBorder) +
-                        equation.substring(leftBorder + 1, rightBorder);
-            }
+                        doOperations(equation.substring(leftBorder, rightBorder + 1));
         }
-        return Integer.parseInt(equation);
+        return equation;
     }
 
 
-    private String toNormForm(Map<String, String[]> p){
+    private String toNormForm(Map<String, String[]> p) {
         HashMap<String, String> variables = new HashMap<>();
         String equation = "";
-        for(String k : p.keySet()){
-            if ("equation".equals(k)){
-                equation = k;
-            }else if (variables.containsKey(p.get(k)[0])){
+        for (String k : p.keySet()) {
+            if ("equation".equals(k)) {
+                equation = p.get(k)[0];
+            } else if (variables.containsKey(p.get(k)[0])) {
                 variables.put(k, variables.get(p.get(k)[0]));
-            }else{
+            } else {
                 variables.put(k, p.get(k)[0]);
             }
         }
-        equation = equation.replace(" ","");
+        equation = equation.replace(" ", "");
         StringJoiner sJoiner = new StringJoiner("");
         String[] elements = equation.split("");
-        for(int i = 0; i < elements.length ; i++) {
+        for (int i = 0; i < elements.length; i++) {
             if (variables.containsKey(elements[i])) {
                 elements[i] = variables.get(elements[i]);
             }
@@ -70,55 +68,57 @@ public class CalcServlet extends HttpServlet {
         return '(' + sJoiner.toString() + ')';
     }
 
-    private String doOperations(String partOfEquation){
+    private String doOperations(String partOfEquation) {
         String result = partOfEquation;
         int i = 0;
-        while(i != -1){
+        while (i != -1) {
             i = findNextOperation(result);
-            Pair<Integer, Integer> p = findBorderOfOperation(result, i);
-            int res = doThisOperation(result.substring(p.getKey() + 1, p.getValue()), result.charAt(i));
-            result = result.substring(0,p.getKey() + 1) + res + result.substring(p.getValue());
+            if (i != -1) {
+                Pair<Integer, Integer> p = findBorderOfOperation(result, i);
+                int res = doThisOperation(result.substring(p.getKey() + 1, p.getValue()), result.charAt(i));
+                result = result.substring(0, p.getKey() + 1) + res + result.substring(p.getValue());
+            }
         }
-        return result;
+        return result.substring(1, result.length() - 1);
     }
 
-    private int findNextOperation(String partOfEquation){
+    private int findNextOperation(String partOfEquation) {
         int firstMul = partOfEquation.indexOf('*');
         int firstDiv = partOfEquation.indexOf('/');
         int i = -1;
-        if (firstMul != -1){
+        if (firstMul != -1) {
             i = firstMul;
-            if (firstDiv != -1) i = (firstMul < firstDiv)? firstMul: firstDiv;
-        }else{
+            if (firstDiv != -1) i = (firstMul < firstDiv) ? firstMul : firstDiv;
+        } else {
             if (firstDiv != -1) i = firstDiv;
         }
-        if (i == -1){
+        if (i == -1) {
             int firstPlus = partOfEquation.indexOf('+');
             int firstMinus = partOfEquation.indexOf('-');
-            if (firstPlus != -1){
+            if (firstPlus != -1) {
                 i = firstPlus;
-                if (firstMinus != -1) i = (firstPlus < firstMinus)? firstPlus: firstMinus;
-            }else{
+                if (firstMinus != -1) i = (firstPlus < firstMinus) ? firstPlus : firstMinus;
+            } else {
                 if (firstMinus != -1) i = firstMinus;
             }
         }
         return i;
     }
 
-    private Pair<Integer,Integer> findBorderOfOperation(String partOfEquation, int i){
+    private Pair<Integer, Integer> findBorderOfOperation(String partOfEquation, int i) {
         int l = i - 1;
-        while ( l >= 0 &&
+        while ( partOfEquation.charAt(l) != '(' &&
                 partOfEquation.charAt(l) != '+' &&
                 partOfEquation.charAt(l) != '-' &&
                 partOfEquation.charAt(l) != '*' &&
                 partOfEquation.charAt(l) != '/') l--;
         int r = i + 1;
-        while ( r < partOfEquation.length() &&
+        while ( partOfEquation.charAt(r) != ')' &&
                 partOfEquation.charAt(r) != '+' &&
                 partOfEquation.charAt(r) != '-' &&
                 partOfEquation.charAt(r) != '*' &&
                 partOfEquation.charAt(r) != '/') r++;
-        return new Pair<>(l,r);
+        return new Pair<>(l, r);
     }
 
     private int doThisOperation(String partOfEquation, char c) {
@@ -132,6 +132,5 @@ public class CalcServlet extends HttpServlet {
         else if (partOfEquation.charAt(i) == '-') result -= rightValue;
         return result;
     }
-
 }
 
