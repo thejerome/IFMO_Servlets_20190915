bb67b6f5390fa763fcdd8f9023837be136fcd40c
Devix Rootix <shukrik27@gmail.com>
2019-10-13 23:12:48 +0300

    Attempt 1

---
+++ src/main/java/CalcServlet.java
@@ -0,0 +1,31 @@
+import calculator.Calculator;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+
+@WebServlet(name = "CalcServlet", urlPatterns = "/calc")
+
+public class CalcServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter pw = response.getWriter();
+        String equation = request.getParameter("equation");
+        Map<String, String[]> params = request.getParameterMap();
+        for (String key : params.keySet()) {
+            String var = params.get(key)[0];
+            if (!key.equals("equation")) {
+                if (1 == var.length() && (var.charAt(0) >= 'a' && var.charAt(0) <= 'z')) {
+                    var = params.get(var)[0];
+                }
+                equation = equation.replace(key, var);
+            }
+        }
+        pw.println(Calculator.eval(equation));
+    }
+}
