5ef5d5cbff8e51b0c281d07fa70500c318cef10f
thejerome <thejerome@mail.ru>
2019-10-13 16:29:37 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,7 @@
 import java.util.LinkedList;
 import java.util.Scanner;
 
+
 public class Calculator {
     public static void main(String[] ars) {
         Scanner Scanner = new Scanner(System.in);
