ece5e8bba97931fd12136d8af2877c2796d49c5d
DamirJann <pilacis@list.ru>
2019-10-18 06:07:52 +0300

    Last fight

--- src/main/java/com/pilacis/web/servlets/SetEquation.java
+++ src/main/java/com/pilacis/web/servlets/SetEquation.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-import java.io.Writer;
 
 @WebServlet(
         name = "CalcEquation",
