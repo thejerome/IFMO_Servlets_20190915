25922dd24d70eda7c5f216d5ca7f04286504a6d9
marycia624 <marycia624@gmail.com>
2019-10-18 16:01:15 +1000

    task-2 done!!!!!

--- src/main/java/com/marycia/web/servlets/ReqFilter.java
+++ src/main/java/com/marycia/web/servlets/ReqFilter.java
@@ -7,8 +7,6 @@ import javax.servlet.ServletResponse;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebFilter (
