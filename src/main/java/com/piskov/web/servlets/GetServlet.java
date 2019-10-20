66f255ade7fa52f33fb885b1cb4a639d7ce57021
Rachelforce <55845125+Rachelforce@users.noreply.github.com>
2019-10-20 20:06:53 +0300

    get

--- src/main/java/com/piskov/web/servlets/GetServlet.java
+++ src/main/java/com/piskov/web/servlets/GetServlet.java
@@ -27,7 +27,7 @@ public class GetServlet extends HttpServlet {
             resp.setStatus(409);
             writer.write("BAD FORMAT!");
         } else {
-            result = CalcService.calculate(expression);
+            result = Service.calculate(expression);
         }
         writer.write(result);
         writer.flush();
