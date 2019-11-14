027b85e1948c3c3192e1a8b8b45f2813bb832c36
ilyamironow <miilig@yandex.ru>
2019-11-14 21:58:37 +0300

    done(fixed after codacy)

--- src/main/java/servlets/Calculator.java
+++ src/main/java/servlets/Calculator.java
@@ -1,14 +1,14 @@
 package servlets;
 
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.*;
+import java.util.Map;
+import java.util.Stack;
 
 
 @WebServlet(
@@ -16,67 +16,48 @@ import java.util.*;
         urlPatterns = {"/calc"}
 )
 public class Calculator extends HttpServlet {
-    Stack<String>  stack = new Stack<>();
-    Stack<String>  nstack = new Stack<>();
-    int prev = 0;
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        String equation = req.getParameter("equation");
-        Map<String, String[]> allMap = req.getParameterMap();
-        int a = 0;char[] modequation;
-        while (a!=2) {
-            modequation = equation.toCharArray();
-            for (int i = 0; i < modequation.length; i++)
-                if (Character.isLetter(modequation[i])){
-                  equation = equation.replaceAll(String.valueOf(modequation[i]),
-                          String.valueOf(allMap.get(String.valueOf(modequation[i]))[0]));
-                }
-            a++;
-        }
-        modequation = equation.toCharArray();
-        for (int i = 0; i < modequation.length; i++)
-            if ('0' <= modequation[i] && modequation[i] <= '9') {
-                int j = 0;
+    private Stack<String>  stack = new Stack<>();
+    private Stack<String>  nstack = new Stack<>();
+    private int prev = 0;
+
+    private void equationtostack(char[] modequation) {
+        for (char c : modequation)
+            if ('0' <= c && c <= '9') {
+                int j;
                 if (!stack.isEmpty() && prev == 1) {
                     if (stack.peek().matches("[-+]?\\d+")) {
                         j = Integer.parseInt(stack.peek().trim());
                         stack.pop();
-                        stack.push(j + String.valueOf(modequation[i]));
+                        stack.push(j + String.valueOf(c));
                         prev = 1;
                     }
                 } else {
-                    stack.push(String.valueOf(modequation[i]));
+                    stack.push(String.valueOf(c));
                     prev = 1;
                 }
             } else {
-                if (modequation[i] == '+' || modequation[i] == '-') {
-                    if (!nstack.isEmpty()) {
-                        if (nstack.peek().equals("+") || nstack.peek().equals("-") ||
-                                nstack.peek().equals("*") || nstack.peek().equals("/")) {
-                            stack.push(String.valueOf(nstack.peek()));
-                            nstack.pop();
-                        }
+                if (c == '+' || c == '-') {
+                    if ((!nstack.isEmpty()) && (nstack.peek().equals("+") || nstack.peek().equals("-") ||
+                            nstack.peek().equals("*") || nstack.peek().equals("/"))) {
+                        stack.push(String.valueOf(nstack.peek()));
+                        nstack.pop();
                     }
-                    nstack.push(String.valueOf(modequation[i]));
+                    nstack.push(String.valueOf(c));
                     prev = 0;
                 }
-                if (modequation[i] == '*' || modequation[i] == '/') {
-                    if (!nstack.isEmpty()) {
-                        if (nstack.peek().equals("*") || nstack.peek().equals("/")) {
-                            stack.push(String.valueOf(nstack.peek()));
-                            nstack.pop();
-                        }
+                if (c == '*' || c == '/') {
+                    if ((!nstack.isEmpty()) && (nstack.peek().equals("*") || nstack.peek().equals("/"))) {
+                        stack.push(String.valueOf(nstack.peek()));
+                        nstack.pop();
                     }
-                    nstack.push(String.valueOf(modequation[i]));
+                    nstack.push(String.valueOf(c));
                     prev = 0;
                 }
-                if (modequation[i] == '(') {
+                if (c == '(') {
                     nstack.push("(");
                     prev = 0;
                 }
-                if (modequation[i] == ')') {
+                if (c == ')') {
                     prev = 0;
                     do {
                         stack.push(String.valueOf(nstack.peek()));
@@ -89,46 +70,68 @@ public class Calculator extends HttpServlet {
             nstack.push(stack.peek());
             stack.pop();
         }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        PrintWriter out = resp.getWriter();
+        String equation = req.getParameter("equation");
+        Map<String, String[]> allMap = req.getParameterMap();
+        int a = 0;char[] modequation;
+        while (a!=2) {
+            modequation = equation.toCharArray();
+            for (char c : modequation)
+                if (Character.isLetter(c)) {
+                    equation = equation.replaceAll(String.valueOf(c),
+                            String.valueOf(allMap.get(String.valueOf(c))[0]));
+                }
+            a++;
+        }
+        modequation = equation.toCharArray();
+        equationtostack(modequation);
         int leftoperand = 0;
         while (!nstack.isEmpty()) {
-        leftoperand = Integer.parseInt(nstack.peek().trim());
-        stack.push(nstack.peek());
-        nstack.pop();
-        int rightoperand;
-        if (!nstack.isEmpty())
-            rightoperand = Integer.parseInt(nstack.peek().trim());
-        else break;
-        stack.push(nstack.peek());
-        nstack.pop();
-        while (nstack.peek().matches("[-+]?\\d+")) {
-            leftoperand = rightoperand;
-            rightoperand = Integer.parseInt(nstack.peek().trim());
+            leftoperand = Integer.parseInt(nstack.peek().trim());
             stack.push(nstack.peek());
             nstack.pop();
-        }
-        switch (nstack.peek()) {
-            case ("+"):
-                leftoperand += rightoperand;
-                break;
-            case ("-"):
-                leftoperand -= rightoperand;
-                break;
-            case ("*"):
-                leftoperand *= rightoperand;
-                break;
-            case ("/"):
-                leftoperand = leftoperand / rightoperand;
-                break;
-        }
-        nstack.pop();
-        nstack.push(String.valueOf(leftoperand));
-        stack.pop();
-        stack.pop();
-        while (!stack.isEmpty()) {
-            nstack.push(stack.peek());
+            int rightoperand;
+            if (!nstack.isEmpty())
+                rightoperand = Integer.parseInt(nstack.peek().trim());
+            else break;
+            stack.push(nstack.peek());
+            nstack.pop();
+            while (nstack.peek().matches("[-+]?\\d+")) {
+                leftoperand = rightoperand;
+                rightoperand = Integer.parseInt(nstack.peek().trim());
+                stack.push(nstack.peek());
+                nstack.pop();
+            }
+            switch (nstack.peek()) {
+                case ("+"):
+                    leftoperand += rightoperand;
+                    break;
+                case ("-"):
+                    leftoperand -= rightoperand;
+                    break;
+                case ("*"):
+                    leftoperand *= rightoperand;
+                    break;
+                case ("/"):
+                    leftoperand = leftoperand / rightoperand;
+                    break;
+                default :
+                    break;
+            }
+            nstack.pop();
+            nstack.push(String.valueOf(leftoperand));
             stack.pop();
+            stack.pop();
+            while (!stack.isEmpty()) {
+                nstack.push(stack.peek());
+                stack.pop();
+            }
         }
-    }
         out.print(leftoperand);
         nstack.clear();
         stack.clear();
