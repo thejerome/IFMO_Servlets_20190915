17e74f103b572b85375983b51fcf494f7050fbd7
Vlad <vlad11-samarin@yandex.ru>
2019-12-12 08:35:16 +0300

    Fixed imports

--- src/main/java/servlet/VariableServlet.java
+++ src/main/java/servlet/VariableServlet.java
@@ -1,6 +1,5 @@
 package servlet;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
