4c07a60126169fc67f83d11166ef47839f746975
faershtein <56654710+faershtein@users.noreply.github.com>
2020-03-28 15:36:33 +0300

    i did it again

--- src/main/java/ExpressionEvaluatorServlet.java
+++ src/main/java/ExpressionEvaluatorServlet.java
@@ -1,5 +1,3 @@
-package com.efimchick.ifmo.web.servlets;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.*;
-import java.util.function.BiFunction;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns = "/calc")
@@ -15,25 +13,12 @@ public class ExpressionEvaluatorServlet extends HttpServlet {
 
     private static final String EXPRESSION_PARAM_NAME = "equation";
 
-    private static final Map<String, BiFunction<Integer, Integer, Integer>> OPERATION_MAP = new HashMap<String, BiFunction<Integer, Integer, Integer>>() {{
-        put("+", Integer::sum);
-        put("-", (a, b) -> a - b);
-        put("*", (a, b) -> a * b);
-        put("/", (a, b) -> {
-            if (b == 0) {
-                throw new IllegalArgumentException("Divide by zero is not allowed!");
-            }
-            return a / b;
-        });
-    }};
-
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String expression = req.getParameter(EXPRESSION_PARAM_NAME);
         String preparedExpression = prepareExpression(expression, req.getParameterMap());
-        int result = evaluate(preparedExpression);
-        resp.getOutputStream().print(preparedExpression + " = " + result);
+        int result = new ExpressionEvaluator().evaluate(preparedExpression);
+        resp.getOutputStream().print(result);
     }
 
     private String prepareExpression(String expression, Map<String, String[]> params) {
@@ -45,60 +30,18 @@ public class ExpressionEvaluatorServlet extends HttpServlet {
                         .collect(Collectors.toList());
 
         for (String key : keys) {
-            preparedString = preparedString.replaceAll(key, params.get(key)[0]);
+            preparedString = preparedString.replaceAll(key, getParamValue(key, params));
         }
         return preparedString;
     }
 
-    private int evaluate(String expression) {
-        char[] arr = expression.toCharArray();
-
-        Stack<Integer> values = new Stack<>();
-        Stack<Character> operations = new Stack<>();
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == ' ') {
-                continue;
-            }
-
-            if (arr[i] >= '0' && arr[i] <= '9') {
-                StringBuilder builder = new StringBuilder();
-                while (i < arr.length && Character.isDigit(arr[i])) {
-                    builder.append(arr[i++]);
-                }
-                i--;
-                values.push(Integer.valueOf(builder.toString()));
-            } else if (arr[i] == '(') {
-                operations.push(arr[i]);
-            } else if (arr[i] == ')') {
-                while (operations.peek() != '(') {
-                    values.push(performOperation(operations.pop(), values.pop(), values.pop()));
-                }
-                operations.pop();
-            } else if (arr[i] == '+' || arr[i] == '-' || arr[i] == '*' || arr[i] == '/') {
-                while (!operations.empty() && hasPrecedence(arr[i], operations.peek())) {
-                    values.push(performOperation(operations.pop(), values.pop(), values.pop()));
-                }
-                operations.push(arr[i]);
-            }
+    private String getParamValue(String key, Map<String, String[]> paramsMap) {
+        String res = paramsMap.get(key)[0];
+        Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+        while (!pattern.matcher(res).matches()) {
+            res = paramsMap.get(res)[0];
         }
-
-        while (!operations.empty()) {
-            values.push(performOperation(operations.pop(), values.pop(), values.pop()));
-        }
-
-        return values.pop();
-    }
-
-    private boolean hasPrecedence(char op1, char op2) {
-        if (op2 == '(' || op2 == ')') {
-            return false;
-        }
-        return (op1 != '*' && op1 != '/') || (op2 != '+' && op2 != '-');
-    }
-
-    private int performOperation(char operation, int b, int a) {
-        return OPERATION_MAP.getOrDefault(String.valueOf(operation), (p, q) -> 0).apply(a, b);
+        return res;
     }
 
 }
